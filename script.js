8c6260b7c1c3952e7d58e7e973656eeda1bdcef7
Ethan Liu <66912828+EL101@users.noreply.github.com>
2026-02-12 11:57:20 -0500

    Create book class, start working on dialog functionality

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 let addBookButton = document.querySelector(".add-button");
 let bookshelf = document.querySelector(".bookshelf");
+let dialog = document.querySelector("#book-form")
 addBookButton.addEventListener("click", (e) => {
-    let newBook = document.createElement("div");
-    newBook.classList.add("book");
-    bookshelf.append(newBook);
+    dialog.showModal()
 });
+const library = []
+function Book(author, title, pages, read, id) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.read = read
+    this.id = id
+}
+
+function addBook(author, title, pages, read) {
+    library.append(new Book(author, title, pages, read, crypto.randomUUID()))
+}
+
+function displayLibrary() {
+    for (let book in library) {
+        let newBook = document.createElement("div");
+        newBook.classList.add("book")
+        bookshelf.append(newBook)
+    }
+}
