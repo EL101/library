09cc2e21ed5d61ed68a9dcc4adfe2b13a5a4c7fd
Ethan Liu <66912828+EL101@users.noreply.github.com>
2026-02-12 21:57:51 -0500

    Style dialog more, start working on dialog funcitonality

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let addBookButton = document.querySelector(".add-button");
 let bookshelf = document.querySelector(".bookshelf");
-let dialog = document.querySelector("#book-form")
+let dialog = document.querySelector("dialog")
+let exitDialog = document.querySelector(".exit")
+let bookForm = document.querySelector(".book-form")
+let bookFormSubmit = document.querySelector(".book-form-submit")
 addBookButton.addEventListener("click", (e) => {
     dialog.showModal()
-});
+    dialog.classList.toggle("dialog-open")
+})
+exitDialog.addEventListener("click", (e) => {
+    dialog.close()
+    dialog.classList.toggle("dialog-open")
+})
+bookFormSubmit.addEventListener("click", e => {
+    if (bookForm.checkValidity()) {
+        let titleInput = document.querySelector("input#title")
+        let authorInput = document.querySelector("input#author")
+        let pageInput = document.querySelector("input#pages")
+        let completedInput = document.querySelector("input#completed")
+        addBook(titleInput.value, authorInput.value, parseInt(pageInput.value), completedInput.checked)
+        exitDialog.click()
+        console.log(library)
+        displayLibrary()
+    } else {
+        bookForm.reportValidity()
+    }
+})
 const library = []
 function Book(author, title, pages, read, id) {
     this.author = author
@@ -14,7 +36,7 @@ function Book(author, title, pages, read, id) {
 }
 
 function addBook(author, title, pages, read) {
-    library.append(new Book(author, title, pages, read, crypto.randomUUID()))
+    library.push(new Book(author, title, pages, read, crypto.randomUUID()))
 }
 
 function displayLibrary() {
