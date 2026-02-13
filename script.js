d998f34b2de7aa43dab21a77c62ce19980bbbb19
Ethan Liu <66912828+EL101@users.noreply.github.com>
2026-02-12 22:34:16 -0500

    Fix dialog done button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let addBookButton = document.querySelector(".add-button");
 let bookshelf = document.querySelector(".bookshelf");
 let dialog = document.querySelector("dialog")
 let exitDialog = document.querySelector(".exit")
-let bookForm = document.querySelector(".book-form")
+let bookForm = document.querySelector("#book-form")
 let bookFormSubmit = document.querySelector(".book-form-submit")
 addBookButton.addEventListener("click", (e) => {
     dialog.showModal()
@@ -13,19 +13,20 @@ exitDialog.addEventListener("click", (e) => {
     dialog.classList.toggle("dialog-open")
 })
 bookFormSubmit.addEventListener("click", e => {
+    e.preventDefault()
     if (bookForm.checkValidity()) {
         let titleInput = document.querySelector("input#title")
         let authorInput = document.querySelector("input#author")
         let pageInput = document.querySelector("input#pages")
         let completedInput = document.querySelector("input#completed")
         addBook(titleInput.value, authorInput.value, parseInt(pageInput.value), completedInput.checked)
-        exitDialog.click()
-        console.log(library)
         displayLibrary()
+        exitDialog.click()
     } else {
         bookForm.reportValidity()
     }
 })
+
 const library = []
 function Book(author, title, pages, read, id) {
     this.author = author
@@ -40,9 +41,10 @@ function addBook(author, title, pages, read) {
 }
 
 function displayLibrary() {
+    bookshelf.innerHTML = ""
     for (let book in library) {
         let newBook = document.createElement("div");
         newBook.classList.add("book")
-        bookshelf.append(newBook)
+        bookshelf.appendChild(newBook)
     }
 }
